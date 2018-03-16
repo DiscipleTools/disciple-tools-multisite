cd511e2592ab95caeb297608e3c316e787852971
Chris Chasm <chris@chasm.solutions>
2018-03-16 13:02:12 -0600

    Set default theme for new blogs.

--- disciple-tools-multisite.php
+++ disciple-tools-multisite.php
@@ -1,26 +1,46 @@
 <?php
 /**
- * Plugin Name: Disciple Tools Multisite
+ * Plugin Name: Disciple Tools - Multisite
  * Plugin URI:  https://github.com/DiscipleTools/disciple-tools-multisite
- * Description: Small plugin to be added to modify a multisite Disciple Tools environment.
+ * Description: Small plugin to be added to modify a multisite "Disciple Tools" environment.
  * Version:     1.0
  */
 
-
 /**
- * Example of wpmu_new_blog usage
+ * Set the new blog theme to Disciple Tools.
  *
- * @param int    $blog_id Blog ID.
- * @param int    $user_id User ID.
- * @param string $domain  Site domain.
- * @param string $path    Site path.
- * @param int    $site_id Site ID. Only relevant on multi-network installs.
- * @param array  $meta    Meta data. Used to set initial site options.
+ * @param $blog_id
+ * @param $user_id
+ * @param $domain
+ * @param $path
+ * @param $site_id
+ * @param $meta
  */
 function dt_new_blog_force_dt_theme( $blog_id, $user_id, $domain, $path, $site_id, $meta ) {
-    dt_write_log($meta);
+    update_blog_option( $blog_id,'template','disciple-tools-theme' );
+    update_blog_option( $blog_id,'stylesheet','disciple-tools-theme' );
 }
-add_action( 'wpmu_new_blog', 'wporg_wpmu_new_blog_example', 10, 6 );
+add_action( 'wpmu_new_blog', 'dt_new_blog_force_dt_theme', 10, 6 );
 
 
-
+/**
+ * Dev functions for easily logging
+ */
+if ( ! function_exists( 'dt_write_log' ) ) {
+    /**
+     * A function to assist development only.
+     * This function allows you to post a string, array, or object to the WP_DEBUG log.
+     *
+     * @param $log
+     */
+    function dt_write_log( $log )
+    {
+        if ( true === WP_DEBUG ) {
+            if ( is_array( $log ) || is_object( $log ) ) {
+                error_log( print_r( $log, true ) );
+            } else {
+                error_log( $log );
+            }
+        }
+    }
+}
