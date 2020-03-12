bf14a707d45e913e74bc86ee7307babf884958fc
Chris Chasm <chris@chasm.solutions>
2020-03-12 14:07:17 -0600

    Added mapbox key management tab

--- disciple-tools-multisite.php
+++ disciple-tools-multisite.php
@@ -65,6 +65,10 @@ if ( is_multisite() ) : // check if system is multi-site, if not do not run.
                 <?php echo ( $tab == 'import' ) ? esc_attr( 'nav-tab-active' ) : ''; ?>">
                     <?php echo esc_attr( 'Import Subsite' ) ?>
                 </a>
+                <a href="<?php echo esc_attr( $link ) . 'mapbox_keys' ?>" class="nav-tab
+                <?php echo ( $tab == 'mapbox_keys' ) ? esc_attr( 'nav-tab-active' ) : ''; ?>">
+                    <?php echo esc_attr( 'Mapbox Keys' ) ?>
+                </a>
 
             </h2>
 
@@ -80,6 +84,10 @@ if ( is_multisite() ) : // check if system is multi-site, if not do not run.
                     break;
                 case "import":
                     $object = new DT_Multisite_Tab_Import_Subsite();
+                    $object->content();
+                    break;
+                case "mapbox_keys":
+                    $object = new DT_Multisite_Tab_Mapbox_Keys();
                     $object->content();
                     break;
 
@@ -227,6 +235,111 @@ if ( is_multisite() ) : // check if system is multi-site, if not do not run.
             <!-- End Box -->
             <?php
         }
+    }
+
+    /**
+     * Class DT_Starter_Tab_Second
+     */
+    class DT_Multisite_Tab_Mapbox_Keys
+    {
+        public function content(){
+            $this->process_post();
+            ?>
+            <div class="wrap">
+                <div id="poststuff">
+                    <div id="post-body" class="metabox-holder columns-2">
+                        <div id="post-body-content">
+                            <!-- Main Column -->
+
+                            <?php $this->list_keys() ?>
+
+                            <!-- End Main Column -->
+                        </div><!-- end post-body-content -->
+                        <div id="postbox-container-1" class="postbox-container">
+                            <!-- Right Column -->
+
+                            <!-- End Right Column -->
+                        </div><!-- postbox-container 1 -->
+                        <div id="postbox-container-2" class="postbox-container">
+                        </div><!-- postbox-container 2 -->
+                    </div><!-- post-body meta box container -->
+                </div><!--poststuff end -->
+            </div><!-- wrap end -->
+            <?php
+        }
+
+        public function process_post() {
+            if ( isset( $_POST['mapbox_nonce'] )
+                && wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['mapbox_nonce'] ) ), 'mapbox' ) ) {
+
+                if ( isset( $_POST['site_id'] ) && ! empty( $_POST['site_id'] ) ) {
+                    $site_id = sanitize_text_field( wp_unslash( $_POST['site_id'] ) );
+
+                    if ( isset( $_POST['mapbox_key'] ) && empty( $_POST['mapbox_key'] ) ) {
+                        update_blog_option( $site_id, 'dt_mapbox_api_key', '' );
+                    } else {
+                        update_blog_option( $site_id, 'dt_mapbox_api_key', sanitize_text_field( wp_unslash( $_POST['mapbox_key'] ) ) );
+                    }
+                }
+
+
+            }
+        }
+
+        public function list_keys(){
+            global $wpdb;
+
+            $sites = $wpdb->get_col( "SELECT blog_id FROM {$wpdb->base_prefix}blogs;" );
+            $list = [];
+            $list['count'] = 0;
+            if ( ! empty( $sites ) ) {
+                foreach ( $sites as $site ) {
+                    if ( get_blog_option( $site, 'stylesheet' ) === 'disciple-tools-theme' ) {
+                        $list[$site] = [];
+                        $list[$site]['url'] = get_blog_option( $site, 'siteurl' );
+                        $list[$site]['mapbox_key'] = get_blog_option( $site, 'dt_mapbox_api_key' );
+                        $list['count']++;
+                    }
+                }
+            }
+
+            ?>
+            <!-- Box -->
+            <table class="widefat striped">
+                <thead>
+                <tr>
+                <th>Site Name (<?php echo $list['count']; ?>)</th>
+                <th>Keys</th>
+                <th></th>
+                </tr>
+                </thead>
+                <tbody>
+
+                <?php if ( ! empty( $list ) ) : unset( $list['count'] ); foreach ( $list as $id => $site ) : ?>
+                    <form method="post">
+                    <tr>
+                        <td style="width:30%;">
+                            <?php echo $site['url'] ?>
+                        </td>
+                        <td>
+                            <?php wp_nonce_field( 'mapbox', 'mapbox_nonce') ?>
+                            <input type="hidden" name="site_id" value="<?php echo $id ?>" />
+                            <input type="text" class="regular-text" style="width:100%;" value="<?php echo $site['mapbox_key'] ?>" name="mapbox_key" placeholder="add mapbox key" />
+                        </td>
+                        <td style="width:10%;">
+                            <button class="button btn" type="submit">Update</button>
+                        </td>
+                    </tr>
+                    </form>
+                <?php endforeach; endif; ?>
+                </tbody>
+            </table>
+            <br>
+            <!-- End Box -->
+            <?php
+        }
+
+
     }
 
     /**
@@ -436,6 +549,27 @@ if ( is_multisite() ) : // check if system is multi-site, if not do not run.
                 }
                 return array_unique( $filter_list );
             } );
+        }
+    }
+
+    if ( ! function_exists( 'dt_write_log' ) ) {
+        // @note Included here because the module can be used independently
+        function dt_write_log( $log ) {
+            if ( true === WP_DEBUG ) {
+                global $dt_write_log_microtime;
+                $now = microtime( true );
+                if ( $dt_write_log_microtime > 0 ) {
+                    $elapsed_log = sprintf( "[elapsed:%5dms]", ( $now - $dt_write_log_microtime ) * 1000 );
+                } else {
+                    $elapsed_log = "[elapsed:-------]";
+                }
+                $dt_write_log_microtime = $now;
+                if ( is_array( $log ) || is_object( $log ) ) {
+                    error_log( $elapsed_log . " " . print_r( $log, true ) );
+                } else {
+                    error_log( "$elapsed_log $log" );
+                }
+            }
         }
     }
 
