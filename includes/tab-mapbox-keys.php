517bf963a350cf388ee024dcdf91bf6c27d2b059
corsacca <corsacca@gmail.com>
2021-02-03 18:38:19 +0100

    Upgrade locations on each multisite

--- includes/tab-mapbox-keys.php
+++ includes/tab-mapbox-keys.php
@@ -28,6 +28,9 @@ class DT_Multisite_Tab_Mapbox_Keys
 
                         <?php $this->default_key() ?>
 
+                        <?php $this->upgrade_locations() ?>
+
+
                         <!-- End Right Column -->
                     </div><!-- postbox-container 1 -->
                     <div id="postbox-container-2" class="postbox-container">
@@ -96,9 +99,11 @@ class DT_Multisite_Tab_Mapbox_Keys
         if ( ! empty( $sites ) ) {
             foreach ( $sites as $site ) {
                 if ( get_blog_option( $site, 'stylesheet' ) === 'disciple-tools-theme' ) {
+                    $dt_setup_options = get_blog_option( $site, "dt_setup_options", [] );
                     $list[$site] = [];
                     $list[$site]['url'] = get_blog_option( $site, 'siteurl' );
                     $list[$site]['mapbox_key'] = get_blog_option( $site, 'dt_mapbox_api_key' );
+                    $list[$site]['locations_upgraded'] = isset( $dt_setup_options["mapbox_upgrade"] );
                     $list['count']++;
                 }
             }
@@ -112,6 +117,7 @@ class DT_Multisite_Tab_Mapbox_Keys
                 <th>Site Name (<?php echo esc_attr( $list['count'] ) ?>)</th>
                 <th>Keys</th>
                 <th></th>
+                <th>Locations Upgraded</th>
             </tr>
             </thead>
             <tbody>
@@ -129,6 +135,9 @@ class DT_Multisite_Tab_Mapbox_Keys
                         </td>
                         <td style="width:10%;">
                             <button class="button btn" type="submit">Update</button>
+                        </td>
+                        <td>
+                            <?php if ( $site['mapbox_key'] ) { echo esc_html( $site["locations_upgraded"] ? "True" : "False" ); } ?>
                         </td>
                     </tr>
                 </form>
@@ -194,6 +203,210 @@ class DT_Multisite_Tab_Mapbox_Keys
                         <button class="button btn" type="submit">Set Default Key</button>
                     </td>
                 </tr>
+                </tbody>
+            </table>
+        </form>
+        <br>
+        <!-- End Box -->
+        <?php
+    }
+
+
+    public function upgrade_locations(){
+
+        ?>
+        <!-- Box -->
+        <form method="GET" action="">
+            <?php wp_nonce_field( 'upgrade_database', 'upgrade_database_nonce' ) ?>
+            <input type="hidden" name="page" value="disciple-tools-multisite" />
+            <input type="hidden" name="loop" value="1" />
+            <input type="hidden" name="tab" value="mapbox_keys" />
+            <table class="widefat striped">
+                <thead>
+                <tr>
+                    <th>Upgrade Locations</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>
+                        <button class="button btn" type="submit" name="upgrade_database">Upgrade Locations</button>
+                    </td>
+                </tr>
+
+                <?php
+                $continue = false;
+                $site_to_update = null;
+                global $wpdb;
+                if ( isset( $_GET['upgrade_database_nonce'] )
+                    && wp_verify_nonce( sanitize_text_field( wp_unslash( $_GET['upgrade_database_nonce'] ) ), 'upgrade_database' )
+                     ) {
+                    $continue = true;
+                }
+
+                if ( $continue ){
+
+                    //find the site we need to update
+                    $sites = $wpdb->get_col( "SELECT blog_id FROM {$wpdb->base_prefix}blogs;" );
+
+                    if ( ! empty( $sites ) ) {
+                        foreach ( $sites as $site ) {
+                            if ( get_blog_option( $site, 'stylesheet' ) === 'disciple-tools-theme' ) {
+                                $dt_setup_options = get_blog_option( $site, "dt_setup_options", [] );
+                                $api_key = get_blog_option( $site, 'dt_mapbox_api_key' );
+                                if ( $api_key && !isset( $dt_setup_options["mapbox_upgrade"] ) ){
+                                    $site_to_update = $site;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if ( $site_to_update ){
+                    switch_to_blog( $site_to_update );
+                    $wpdb->dt_location_grid_meta = $wpdb->prefix . 'dt_location_grid_meta';
+                    $wpdb->dt_location_grid = $wpdb->prefix . 'dt_location_grid';
+                    require_once( get_template_directory() . '/dt-mapping/mapping-admin.php' );
+                    if ( !method_exists( DT_Mapping_Module_Admin::instance(), "get_record_count_with_no_location_meta" ) ){
+                        ?><tr><td><strong>This feature requires D.T v1.0.2 or later</strong><br><?php
+                        return;
+                    }
+                    $location_wo_meta = DT_Mapping_Module_Admin::instance()->get_record_count_with_no_location_meta();
+                    $limit = 100;
+
+                    $count = $location_wo_meta;
+
+                    $user_location_wo_meta = DT_Mapping_Module_Admin::instance()->get_user_count_with_no_location_meta();
+                    $user_count = $user_location_wo_meta;
+
+                    if ( empty( $count ) && empty( $user_count ) ){
+                        $dt_setup_options = get_option( "dt_setup_options", [] );
+                        $dt_setup_options["mapbox_upgrade"] = true;
+                        update_option( "dt_setup_options", $dt_setup_options );
+                    }
+                    $greater_than_limit = true;
+
+
+                    ?>
+
+                    <?php if ( !empty( $count ) || !empty( $user_count ) ) : ?>
+                    <tr>
+                        <td>
+                            <strong><?php echo esc_html( get_site_url() ); ?></strong><br>
+                            <strong>Processing ( <?php echo esc_attr( $count + $user_count ) ?> ) </strong><br>
+                            <span><img src="<?php echo esc_url( trailingslashit( get_stylesheet_directory_uri() ) ) ?>spinner.svg" width="22px" alt="spinner "/></span><br>
+                            <?php
+
+                            require_once( get_template_directory() . '/dt-mapping/geocode-api/location-grid-geocoder.php' );
+                            require_once( get_template_directory() . '/dt-mapping/location-grid-meta.php' );
+                            require_once( get_template_directory() . '/dt-mapping/mapping-queries.php' );
+                            global $wpdb;
+                            if ( !empty( $count )){
+                                // Insert processing with offset
+                                $geocoder = new Location_Grid_Geocoder();
+                                $query = $wpdb->get_results( $wpdb->prepare( "
+                                    SELECT *
+                                    FROM $wpdb->postmeta
+                                    WHERE meta_key = 'location_grid'
+                                    AND meta_id NOT IN (
+                                        SELECT DISTINCT( postmeta_id_location_grid )
+                                        FROM $wpdb->dt_location_grid_meta
+                                    )
+                                    AND meta_value >= 100000000
+                                    LIMIT %d",
+                                    $limit
+                                ), ARRAY_A );
+
+                                if ( !empty( $query ) ){
+                                    foreach ( $query as $row ){
+                                        $grid = $geocoder->query_by_grid_id( $row["meta_value"] );
+                                        if ( $grid ){
+                                            $location_meta_grid = [];
+
+                                            Location_Grid_Meta::validate_location_grid_meta( $location_meta_grid );
+                                            $location_meta_grid['post_id'] = $row['post_id'];
+                                            $location_meta_grid['post_type'] = get_post_type( $row['post_id'] );
+                                            $location_meta_grid['grid_id'] = $row['meta_value'];
+                                            $location_meta_grid['lng'] = $grid["longitude"];
+                                            $location_meta_grid['lat'] = $grid["latitude"];
+                                            $location_meta_grid['level'] = $grid["level_name"];
+                                            $location_meta_grid['label'] = $geocoder->_format_full_name( $grid );
+
+                                            $potential_error = Location_Grid_Meta::add_location_grid_meta( $row['post_id'], $location_meta_grid, $row['meta_id'] );
+
+                                            echo esc_html( $location_meta_grid['label'] ) . '<br>';
+                                        }
+                                    }
+                                }
+                            } elseif ( !empty( $user_count ) ){
+                                $geocoder = new Location_Grid_Geocoder();
+                                $query = $wpdb->get_results( $wpdb->prepare( "
+                                    SELECT *
+                                    FROM $wpdb->usermeta
+                                    WHERE meta_key = %s
+                                    AND umeta_id NOT IN (
+                                        SELECT DISTINCT( postmeta_id_location_grid )
+                                        FROM $wpdb->dt_location_grid_meta)
+                                    LIMIT %d",
+                                    $wpdb->prefix . 'location_grid',
+                                    $limit
+                                ), ARRAY_A);
+                                if ( ! empty( $query ) ) {
+                                    foreach ( $query as $row ) {
+                                        $grid = $geocoder->query_by_grid_id( $row["meta_value"] );
+                                        if ( $grid ) {
+                                            $location_meta_grid = [];
+
+                                            Location_Grid_Meta::validate_location_grid_meta( $location_meta_grid );
+                                            $location_meta_grid['post_id'] = $row['user_id'];
+                                            $location_meta_grid['post_type'] = 'users';
+                                            $location_meta_grid['grid_id'] = $row['meta_value'];
+                                            $location_meta_grid['lng'] = $grid["longitude"];
+                                            $location_meta_grid['lat'] = $grid["latitude"];
+                                            $location_meta_grid['level'] = $grid["level_name"];
+                                            $location_meta_grid['label'] = $geocoder->_format_full_name( $grid );
+
+                                            $potential_error = Location_Grid_Meta::add_user_location_grid_meta( $row['user_id'], $location_meta_grid, $row['umeta_id'] );
+
+                                            echo esc_html( $location_meta_grid['label'] ) . '<br>';
+                                        }
+                                    }
+                                }
+                            }
+
+                            ?>
+                        <td>
+                    <tr>
+
+                    <?php else : ?>
+                        <tr>
+                            <td>
+                                <strong>Loading next site</strong><br>
+                                <span><img src="<?php echo esc_url( trailingslashit( get_stylesheet_directory_uri() ) ) ?>spinner.svg" width="22px" alt="spinner "/></span><br>
+                            </td>
+                        </tr>
+                    <?php endif; // loop_again
+                    restore_current_blog();
+                    ?><script type="text/javascript">
+                        function nextpage() {
+                            location.href = "<?php echo esc_url( network_admin_url() ) ?>admin.php?page=disciple-tools-multisite&tab=mapbox_keys&upgrade_database_nonce=<?php echo esc_attr( wp_create_nonce( 'upgrade_database' ) ) ?>&loop=<?php echo esc_attr( $greater_than_limit ) ?>";
+                        }
+                        setTimeout( "nextpage()", 1500 );
+                    </script>
+                    <?php
+                } else if ($continue) {
+                    ?>
+                        <script type="text/javascript">
+                        function nextpage() {
+                            location.href = "<?php echo esc_url( network_admin_url() ) ?>admin.php?page=disciple-tools-multisite&tab=mapbox_keys";
+                        }
+                        setTimeout( "nextpage()", 100 );
+                        </script>
+                    <?php
+                }
+                ?>
+
                 </tbody>
             </table>
         </form>
