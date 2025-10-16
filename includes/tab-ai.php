e74fc79765e66878fd54a8b339773befe974b271
kodinkat <me@karlkatewu.com>
2025-10-16 17:12:35 +0100

    Enhance AI settings in DT_Multisite_Tab_AI
    - Added support for selecting LLM providers and their respective chat and transcript paths in the settings form.
    - Updated JavaScript to dynamically populate chat and transcript paths based on the selected provider.
    - Improved form structure for better clarity between chat and transcription model configurations.

--- includes/tab-ai.php
+++ includes/tab-ai.php
@@ -39,9 +39,13 @@ class DT_Multisite_Tab_AI
             && wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['ai_nonce'] ) ), 'ai' ) ) {
 
             $settings = [
+                'llm_provider' => sanitize_text_field( wp_unslash( $_POST['dt_ai_llm_providers'] ?? 'predictionguard' ) ),
+                'llm_provider_chat_path' => sanitize_text_field( wp_unslash( $_POST['dt_ai_llm_provider_chat_paths'] ?? 'chat_complete' ) ),
                 'llm_endpoint' => sanitize_text_field( wp_unslash( $_POST['dt_ai_llm_endpoint'] ?? '' ) ),
                 'llm_api_key' => sanitize_text_field( wp_unslash( $_POST['dt_ai_llm_api_key'] ?? '' ) ),
                 'llm_model' => sanitize_text_field( wp_unslash( $_POST['dt_ai_llm_model'] ?? '' ) ),
+                'transcript_llm_provider' => sanitize_text_field( wp_unslash( $_POST['dt_ai_transcript_llm_providers'] ?? 'predictionguard' ) ),
+                'transcript_llm_provider_transcript_path' => sanitize_text_field( wp_unslash( $_POST['dt_ai_transcript_llm_provider_transcript_paths'] ?? 'audio_transcript' ) ),
                 'transcript_llm_endpoint' => sanitize_text_field( wp_unslash( $_POST['dt_ai_transcript_llm_endpoint'] ?? '' ) ),
                 'transcript_llm_api_key' => sanitize_text_field( wp_unslash( $_POST['dt_ai_transcript_llm_api_key'] ?? '' ) ),
                 'transcript_llm_model' => sanitize_text_field( wp_unslash( $_POST['dt_ai_transcript_llm_model'] ?? '' ) )
@@ -63,13 +67,25 @@ class DT_Multisite_Tab_AI
 
     public function list_keys( $processed ){
         $network_settings = get_site_option( 'DT_AI_connection_settings', [
+            'llm_provider' => 'predictionguard',
+            'llm_provider_chat_path' => 'chat_complete',
             'llm_endpoint' => '',
             'llm_api_key' => '',
             'llm_model' => '',
+            'transcript_llm_provider' => 'predictionguard',
+            'transcript_llm_provider_transcript_path' => 'audio_transcript',
             'transcript_llm_endpoint' => '',
             'transcript_llm_api_key' => '',
             'transcript_llm_model' => ''
         ] );
+
+        $ai_providers = apply_filters( 'dt_ai_providers', [] );
+
+        $selected_ai_provider = $network_settings['llm_provider'] ?? 'predictionguard';
+        $selected_ai_provider_chat_path = $network_settings['llm_provider_chat_path'] ?? 'chat_complete';
+
+        $selected_ai_transcript_provider = $network_settings['transcript_llm_provider'] ?? 'predictionguard';
+        $selected_ai_transcript_provider_chat_path = $network_settings['transcript_llm_provider_transcript_path'] ?? 'audio_transcript';
 
         if ( isset( $processed['is_update'], $processed['updated'] ) && $processed['is_update'] ) {
             ?>
@@ -93,10 +109,42 @@ class DT_Multisite_Tab_AI
                 <tbody>
                     <tr>
                         <td style="width:30%;">
+                            Provider
+                        </td>
+                        <td>
+                            <select id="dt_ai_llm_providers" name="dt_ai_llm_providers" style="width:50%; vertical-align: top;">
+                                <?php
+                                foreach ( $ai_providers as $provider_key => $provider ) {
+
+                                    // Exclude providers with no valid paths.
+                                    if ( !empty( $provider['paths']['chat'] ) ) {
+                                        $selected = ($selected_ai_provider == $provider_key) ? 'selected="selected"' : '';
+                                        ?>
+                                        <option value="<?php echo esc_attr( $provider_key ) ?>" <?php echo esc_attr( $selected ) ?>><?php echo esc_attr( $provider['label'] ) ?></option>
+                                        <?php
+                                    }
+                                }
+                                ?>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width:30%;">
                             Endpoint
                         </td>
                         <td>
-                            <input type="text" class="regular-text" style="width:100%;" value="<?php echo esc_attr( $network_settings['llm_endpoint'] ) ?>" name="dt_ai_llm_endpoint" placeholder="Add Chat LLM Endpoint" />
+                            <input type="text" class="regular-text" style="width:50%; vertical-align: top;" value="<?php echo esc_attr( $network_settings['llm_endpoint'] ) ?>" id="dt_ai_llm_endpoint" name="dt_ai_llm_endpoint" placeholder="Add Chat LLM Endpoint" />
+
+                            <select id="dt_ai_llm_provider_chat_paths" name="dt_ai_llm_provider_chat_paths" style="width:48%; vertical-align: top;">
+                                <?php
+                                foreach ( $ai_providers[ $selected_ai_provider ]['paths']['chat'] as $path_key => $path ) {
+                                    $selected = ($selected_ai_provider_chat_path == $path_key) ? 'selected="selected"' : '';
+                                    ?>
+                                    <option value="<?php echo esc_attr( $path_key ) ?>" <?php echo esc_attr( $selected ) ?>><?php echo esc_attr( $path ) ?></option>
+                                    <?php
+                                }
+                                ?>
+                            </select>
                         </td>
                     </tr>
                     <tr>
@@ -112,7 +160,7 @@ class DT_Multisite_Tab_AI
                             Model
                         </td>
                         <td>
-                            <input type="text" class="regular-text" style="width:100%;" value="<?php echo esc_attr( $network_settings['llm_model'] ) ?>" name="dt_ai_llm_model" placeholder="Add Chat LLM Model" />
+                            <input type="text" class="regular-text" style="width:100%;" value="<?php echo esc_attr( $network_settings['llm_model'] ) ?>" id="dt_ai_llm_model" name="dt_ai_llm_model" placeholder="Add Chat LLM Model" />
                         </td>
                     </tr>
                     <tr>
@@ -134,10 +182,42 @@ class DT_Multisite_Tab_AI
                 <tbody>
                 <tr>
                     <td style="width:30%;">
+                        Provider
+                    </td>
+                    <td>
+                        <select id="dt_ai_transcript_llm_providers" name="dt_ai_transcript_llm_providers" style="width:50%; vertical-align: top;">
+                            <?php
+                            foreach ( $ai_providers as $provider_key => $provider ) {
+
+                                // Exclude providers with no valid paths.
+                                if ( !empty( $provider['paths']['transcript'] ) ) {
+                                    $selected = ($selected_ai_transcript_provider == $provider_key) ? 'selected="selected"' : '';
+                                    ?>
+                                    <option value="<?php echo esc_attr( $provider_key ) ?>" <?php echo esc_attr( $selected ) ?>><?php echo esc_attr( $provider['label'] ) ?></option>
+                                    <?php
+                                }
+                            }
+                            ?>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="width:30%;">
                         Endpoint
                     </td>
                     <td>
-                        <input type="text" class="regular-text" style="width:100%;" value="<?php echo esc_attr( $network_settings['transcript_llm_endpoint'] ) ?>" name="dt_ai_transcript_llm_endpoint" placeholder="Add Transcript LLM Endpoint" />
+                        <input type="text" class="regular-text" style="width:50%; vertical-align: top;" value="<?php echo esc_attr( $network_settings['transcript_llm_endpoint'] ) ?>" id="dt_ai_transcript_llm_endpoint" name="dt_ai_transcript_llm_endpoint" placeholder="Add Transcript LLM Endpoint" />
+
+                        <select id="dt_ai_transcript_llm_provider_transcript_paths" name="dt_ai_transcript_llm_provider_transcript_paths" style="width:48%; vertical-align: top;">
+                            <?php
+                            foreach ( $ai_providers[ $selected_ai_transcript_provider ]['paths']['transcript'] as $path_key => $path ) {
+                                $selected = ($selected_ai_transcript_provider_chat_path == $path_key) ? 'selected="selected"' : '';
+                                ?>
+                                <option value="<?php echo esc_attr( $path_key ) ?>" <?php echo esc_attr( $selected ) ?>><?php echo esc_attr( $path ) ?></option>
+                                <?php
+                            }
+                            ?>
+                        </select>
                     </td>
                 </tr>
                 <tr>
@@ -153,7 +233,7 @@ class DT_Multisite_Tab_AI
                         Model
                     </td>
                     <td>
-                        <input type="text" class="regular-text" style="width:100%;" value="<?php echo esc_attr( $network_settings['transcript_llm_model'] ) ?>" name="dt_ai_transcript_llm_model" placeholder="Add Transcript LLM Model" />
+                        <input type="text" class="regular-text" style="width:100%;" value="<?php echo esc_attr( $network_settings['transcript_llm_model'] ) ?>" id="dt_ai_transcript_llm_model" name="dt_ai_transcript_llm_model" placeholder="Add Transcript LLM Model" />
                     </td>
                 </tr>
                 <tr>
@@ -167,6 +247,107 @@ class DT_Multisite_Tab_AI
             </table>
         </form>
         <br>
+        <script>
+            jQuery(document).ready(function() {
+                const aiProviders = [<?php echo json_encode( $ai_providers ) ?>][0];
+
+                /**
+                 * Chat Model
+                 */
+
+                // Function to update the chat paths dropdown based on selected provider
+                function updateChatElements(selectedProvider) {
+                    const chatEndpoint = jQuery('#dt_ai_llm_endpoint');
+                    const chatPathsSelect = jQuery('#dt_ai_llm_provider_chat_paths');
+                    const chatModel = jQuery('#dt_ai_llm_model');
+                    const aiProvider = aiProviders[selectedProvider];
+
+                    // Ensure we have a valid ai provider
+                    if ( aiProvider ) {
+
+                        // Get the paths for the selected provider
+                        if ( aiProvider?.paths?.chat ) {
+                            const chatPaths = aiProvider.paths.chat;
+
+                            // Clear existing options
+                            chatPathsSelect.empty();
+
+                            // Add options for each path
+                            jQuery.each(chatPaths, function(pathKey, pathValue) {
+                                chatPathsSelect.append(
+                                    jQuery('<option></option>')
+                                    .attr('value', pathKey)
+                                    .text(pathValue)
+                                );
+                            });
+                        }
+
+                        // Set default endpoint for the selected provider
+                        if ( aiProvider?.endpoints?.chat && aiProvider.endpoints.chat.length > 0 ) {
+                            chatEndpoint.val( aiProvider.endpoints.chat[0] );
+                        }
+
+                        // Set default model for the selected provider
+                        if ( aiProvider?.models?.chat && aiProvider.models.chat.length > 0 ) {
+                            chatModel.val( aiProvider.models.chat[0] );
+                        }
+                    }
+                }
+
+                // Add change event listener to provider select
+                jQuery('#dt_ai_llm_providers').on('change', function() {
+                    updateChatElements( jQuery(this).val() );
+                });
+
+                /**
+                 * Transcription Model
+                 */
+
+                // Function to update the transcription paths dropdown based on selected provider
+                function updateTranscriptElements(selectedProvider) {
+                    const transcriptEndpoint = jQuery('#dt_ai_transcript_llm_endpoint');
+                    const transcriptPathsSelect = jQuery('#dt_ai_transcript_llm_provider_transcript_paths');
+                    const transcriptModel = jQuery('#dt_ai_transcript_llm_model');
+                    const aiProvider = aiProviders[selectedProvider];
+
+                    // Ensure we have a valid ai provider
+                    if ( aiProvider ) {
+
+                        // Get the paths for the selected provider
+                        if ( aiProvider?.paths?.transcript ) {
+                            const transcriptPaths = aiProvider.paths.transcript;
+
+                            // Clear existing options
+                            transcriptPathsSelect.empty();
+
+                            // Add options for each path
+                            jQuery.each(transcriptPaths, function(pathKey, pathValue) {
+                                transcriptPathsSelect.append(
+                                    jQuery('<option></option>')
+                                    .attr('value', pathKey)
+                                    .text(pathValue)
+                                );
+                            });
+                        }
+
+                        // Set default endpoint for the selected provider
+                        if ( aiProvider?.endpoints?.transcript && aiProvider.endpoints.transcript.length > 0 ) {
+                            transcriptEndpoint.val( aiProvider.endpoints.transcript[0] );
+                        }
+
+                        // Set default model for the selected provider
+                        if ( aiProvider?.models?.transcript && aiProvider.models.transcript.length > 0 ) {
+                            transcriptModel.val( aiProvider.models.transcript[0] );
+                        }
+                    }
+                }
+
+                // Add change event listener to provider select
+                jQuery('#dt_ai_transcript_llm_providers').on('change', function() {
+                    updateTranscriptElements( jQuery(this).val() );
+                });
+            });
+        </script>
         <!-- End Box -->
         <?php
     }
